1ef85ee39347bd051e736d7f3a9fab2769eeb914
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-03-20 14:37:51 +0700

    chore: update build_push_image script

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,18 @@
 #/bin/sh
 
-# build docker image dengan namespace ghcr.io/kido1611/item-app dan versi v1
-docker build -t ghcr.io/kido1611/item-app:v1 .
+# build docker image dengan item-app dan versi v1
+docker build -t item-app:v1 .
+
+# melihat semua docker image yang ada di lokal
+docker image ls
+
+# menyalin docker image dengan namespace yang sesuai supaya bisa dipush ke container registry (Github)
+# source item-app:v1
+# target ghcr.io/kido1611/item-app:v1
+docker tag item-app:v1 ghcr.io/kido1611/item-app:v1
+
+# login container registry ghcr.io
+echo $PASSWORD_GITHUB_PACKAGES | docker login -u kido1611 --password-stdin ghcr.io
 
 # push container image
 docker push ghcr.io/kido1611/item-app:v1
