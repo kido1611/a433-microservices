0826cccb76cfb1d50e109480738a00ad4689c9c7
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-03-22 15:34:34 +0700

    chore: add comment on build_push_image script

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,5 +1,23 @@
 #/bin/sh
 
+# Untuk menampilkan tulisan title
+print_title() {
+  # print kata/kalimat dengan text bold, background merah, berwarna putih
+  # 1  : Bold
+  # 41 : Background Red
+  # 37 : Text White
+  # 0  : Reset
+  echo -e "\e[1;41;37m $1 \e[0m"
+}
+
+# build container image
+print_title "Build container image"
 docker build -t ghcr.io/kido1611/karsajobs-ui:latest .
 
+# login container registry ghcr.io
+print_title "Login to Container Registry"
+echo $PASSWORD_GITHUB_PACKAGES | docker login -u kido1611 --password-stdin ghcr.io
+
+# push container image to container registry (Github Packages)
+print_title "Push container image"
 docker push ghcr.io/kido1611/karsajobs-ui:latest
